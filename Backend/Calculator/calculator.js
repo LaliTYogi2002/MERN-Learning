cf969f0080d1ba0e31a4e2e3afe1b40b3b3c3ccb
Lalchand Yogi <lalityogi2506666@gmail.com>
2023-07-08 23:42:13 +0530

    Section 24 BMI Calculator Done

--- Backend/Calculator/calculator.js
+++ Backend/Calculator/calculator.js
@@ -12,6 +12,20 @@ app.post("/",function(req,res){
     res.send(`Addition is ${ans}`)
 })
 
+app.get("/bmicalculator",function(req,res){
+
+    res.sendFile(__dirname+"/bmiCalculator.html");
+})
+
+app.post("/bmicalculator",function(req,res){
+    var weight = parseFloat(req.body.weight)
+    var height = parseFloat(req.body.height)
+    var bmi = weight/(height*height) ;
+
+    res.send(`${bmi}`)
+
+})
+
 app.listen(3000,function(){
     console.log("Server Started on 3000 port");
 })
